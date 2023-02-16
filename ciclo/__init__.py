068e0372f070ba62f9195e7b93cd97733f5adcec
Cristian Garcia <cgarcia.e88@gmail.com>
2023-02-16 15:27:02 +0000

    refactor to common

--- ciclo/__init__.py
+++ ciclo/__init__.py
@@ -12,21 +12,21 @@ from .callbacks import noop as noop
 from .callbacks import tqdm_bar as tqdm_bar
 from .callbacks import wandb_logger as wandb_logger
 from .logging import History, Logs
-from .loops.fit_loop import ON_EPOCH_END as on_epoch_end
-from .loops.fit_loop import ON_TEST_BATCH_BEGIN as on_test_batch_begin
-from .loops.fit_loop import ON_TEST_BATCH_END as on_test_batch_end
-from .loops.fit_loop import ON_TEST_BEGIN as on_test_begin
-from .loops.fit_loop import ON_TEST_END as on_test_end
-from .loops.fit_loop import ON_TRAIN_BATCH_BEGIN as on_train_batch_begin
-from .loops.fit_loop import ON_TRAIN_BATCH_END as on_train_batch_end
-from .loops.fit_loop import ON_TRAIN_BEGIN as on_train_begin
-from .loops.fit_loop import ON_TRAIN_END as on_train_end
-from .loops.fit_loop import RESET_STEP as reset_step
-from .loops.fit_loop import TEST_STEP as test_step
-from .loops.fit_loop import TRAIN_STEP as train_step
-from .loops.fit_loop import predict_loop as predict_loop
-from .loops.fit_loop import test_loop as test_loop
-from .loops.fit_loop import train_loop as train_loop
+from .loops.common import ON_EPOCH_END as on_epoch_end
+from .loops.common import ON_TEST_BATCH_BEGIN as on_test_batch_begin
+from .loops.common import ON_TEST_BATCH_END as on_test_batch_end
+from .loops.common import ON_TEST_BEGIN as on_test_begin
+from .loops.common import ON_TEST_END as on_test_end
+from .loops.common import ON_TRAIN_BATCH_BEGIN as on_train_batch_begin
+from .loops.common import ON_TRAIN_BATCH_END as on_train_batch_end
+from .loops.common import ON_TRAIN_BEGIN as on_train_begin
+from .loops.common import ON_TRAIN_END as on_train_end
+from .loops.common import RESET_STEP as reset_step
+from .loops.common import TEST_STEP as test_step
+from .loops.common import TRAIN_STEP as train_step
+from .loops.common import predict_loop as predict_loop
+from .loops.common import test_loop as test_loop
+from .loops.common import train_loop as train_loop
 from .loops.loop import LoopCallbackBase, LoopElement, LoopState, loop
 from .schedules import always as always
 from .schedules import every as every
